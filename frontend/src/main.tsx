ef4b856a6560f872915263eb87b29b97986f6212
Botond Csereklye <botond.csereklye@gmail.com>
2026-05-28 08:36:10 +0200

    Design responsive security dashboard interface

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/space-grotesk/400.css'
+import '@fontsource/space-grotesk/500.css'
+import '@fontsource/space-grotesk/700.css'
+import '@fontsource/ibm-plex-mono/400.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
